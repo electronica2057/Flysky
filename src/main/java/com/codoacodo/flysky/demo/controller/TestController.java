1ca889b720835676a0187638b92a94ab7e6b6f9b
Jbecerra01 <jbecerra@frba.utn.edu.ar>
2023-06-24 15:16:38 -0300

    test

---
+++ src/main/java/com/codoacodo/flysky/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.codoacodo.flysky.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hola";
+    }
+}
