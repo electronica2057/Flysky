de916e669c10ea1baacd1db86e6c0e709cce1c31
Nicolas Coronel <coronelnicolas77@hotmail.com>
2023-07-02 15:16:43 -0300

    Se agrego para hacer una reserva(reservaServiceImpl,ReservaRequestDto)

---
+++ src/test/java/com/codoacodo/flysky/demo/service/VueloServiceTest.java
@@ -0,0 +1,44 @@
+package com.codoacodo.flysky.demo.service;
+
+import com.codoacodo.flysky.demo.dto.response.VueloDTO;
+import com.codoacodo.flysky.demo.repository.VueloRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+@SpringBootTest
+public class VueloServiceTest {
+
+    @Autowired
+    VueloService vueloService;
+
+    @Autowired
+    VueloRepository vueloRepository;
+
+    @Test
+    @DisplayName("US0001-ListaDeVuelosDisponibles")
+    void buscarVuelosOkTest () {
+        //ARRANGE
+        List<VueloDTO> vuelos = new ArrayList<>();
+        vuelos.add(new VueloDTO (TRUE,50, "Aerolineas Argentinas", "2023-06-25 23:53:30", "2023-06-25 23:53:30", 15000, "Buenos Aires", "Uruguay"));
+        vuelos.add(new VueloDTO (TRUE,50, "Aerolineas Uruguayas", "2023-06-25 23:53:30", "2023-06-25 23:53:30", 15000, "Buenos Aires", "Uruguay"));
+
+
+
+        //ACT
+        List<VueloDTO> result = vueloService.obtenerVuelosDisponibles();
+        //ASSERT
+        assertEquals(vuelos,result);
+    }
+
+
+
+
+
+}*/
