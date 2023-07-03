c35f3406aeb73a411193c4295e7cfafa9b54abb0
Mariano Favre <marianofavre79@yahoo.com.ar>
2023-07-03 12:41:50 -0300

    Modificar data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
 INSERT INTO usuario(telefono, nombre_usuario, tipo_usuario)
 VALUES (156453,'Miguel','CLIENTE'),
        (654896,'Carlos', 'AGENTE_DE_VENTAS'),
-       (984866,'Juan', 'ADMINISTRADOR');
+       (984866,'Juan', 'ADMINISTRADOR'),
+       (666666,'Mariano','CLIENTE');
 
 INSERT INTO vuelo(disponible, capacidad, aerolinea, horario_partida, horario_llegada, precio, origen, destino)
 VALUES (TRUE,50,'Aerolineas Argentinas','2023-06-25 23:53:30','2023-06-25 23:53:30', 15000, 'Buenos Aires', 'Uruguay'),
@@ -12,7 +13,9 @@ VALUES (TRUE,50,'Aerolineas Argentinas','2023-06-25 23:53:30','2023-06-25 23:53:
 INSERT INTO reserva(tipo_pago, usuario_id, vuelo_id, monto_pagar, fecha_reserva)
 VALUES ('TARJETA_CREDITO',1,1,1500,'2023-06-25 23:53:30'),
        ('TRANSFERENCIA_BANCARIA',1,2,2000,'2023-06-29 18:33:20'),
-       ('EFECTIVO',1,2,3000,'2023-06-29 18:33:20');
+       ('EFECTIVO',1,2,3000,'2023-06-29 18:33:20'),
+       ('EFECTIVO',4,4,1700,'2023-06-30 18:33:20');
+
 
 INSERT INTO butaca(disponible, posicion, vuelo_id)
 VALUES (TRUE, 'AE04', 1),
