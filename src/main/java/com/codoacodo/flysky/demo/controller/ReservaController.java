06cd29d53b7a76ce8359b81069d814ae75e14e6f
Mariano Favre <marianofavre79@yahoo.com.ar>
2023-07-05 10:28:52 -0300

    Endpoint para reservarVuelo

--- src/main/java/com/codoacodo/flysky/demo/controller/ReservaController.java
+++ src/main/java/com/codoacodo/flysky/demo/controller/ReservaController.java
@@ -4,12 +4,10 @@ import com.codoacodo.flysky.demo.dto.request.ReservaVueloDTO;
 import com.codoacodo.flysky.demo.service.ReservaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/v1/reserva")
 public class ReservaController {
 
     private final ReservaService reservaService;
@@ -20,7 +18,7 @@ public class ReservaController {
 
 
     //Ver si es post o put ya que modifica la disponibilidad de butaca y vuelo.
-    @PutMapping("/nuevaReserva/{nombreUsuarioTipoCliente}")
+    @PutMapping("/crear/{nombreUsuarioTipoCliente}")
     public ResponseEntity<?> reservarVuelo(@PathVariable String nombreUsuarioTipoCliente,
                                            @RequestBody ReservaVueloDTO reservaVueloDTO) {
         return new ResponseEntity<>(reservaService.reservarVuelo(nombreUsuarioTipoCliente, reservaVueloDTO), HttpStatus.OK);
