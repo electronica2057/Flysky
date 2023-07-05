1259155af7f146cbcde833a464977cef0308725a
martinmetallo <martinmetallo@gmail.com>
2023-07-04 22:45:53 -0300

    Test unitario

--- src/test/java/com/codoacodo/flysky/demo/service/VueloServiceTest.java
+++ src/test/java/com/codoacodo/flysky/demo/service/VueloServiceTest.java
@@ -1,44 +1,62 @@
 package com.codoacodo.flysky.demo.service;
 
 import com.codoacodo.flysky.demo.dto.response.VueloDTO;
+import com.codoacodo.flysky.demo.model.entity.VueloEntity;
 import com.codoacodo.flysky.demo.repository.VueloRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-/*
-@SpringBootTest
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class VueloServiceTest {
-
-    @Autowired
-    VueloService vueloService;
-
-    @Autowired
-    VueloRepository vueloRepository;
 
     @Test
     @DisplayName("US0001-ListaDeVuelosDisponibles")
-    void buscarVuelosOkTest () {
-        //ARRANGE
-        List<VueloDTO> vuelos = new ArrayList<>();
-        vuelos.add(new VueloDTO (TRUE,50, "Aerolineas Argentinas", "2023-06-25 23:53:30", "2023-06-25 23:53:30", 15000, "Buenos Aires", "Uruguay"));
-        vuelos.add(new VueloDTO (TRUE,50, "Aerolineas Uruguayas", "2023-06-25 23:53:30", "2023-06-25 23:53:30", 15000, "Buenos Aires", "Uruguay"));
+    void buscarVuelosOkTest() {
+        // Arrange
+        VueloRepository vueloRepository = Mockito.mock(VueloRepository.class);
+        VueloService vueloService = new VueloServiceImpl(vueloRepository);
 
+        List<VueloEntity> vueloEntities = new ArrayList<>();
+        VueloEntity vueloEntity1 = new VueloEntity();
+        vueloEntity1.setDisponible(true);
+        vueloEntity1.setCapacidad(50);
+        vueloEntity1.setAerolinea("Aerolineas Argentinas");
+        vueloEntity1.setHorarioPartida(LocalDateTime.parse("2023-06-25T23:53:30"));
+        vueloEntity1.setHorarioLlegada(LocalDateTime.parse("2023-06-25T23:53:30"));
+        vueloEntity1.setPrecio(15000.0);
+        vueloEntity1.setOrigen("Buenos Aires");
+        vueloEntity1.setDestino("Uruguay");
+        vueloEntities.add(vueloEntity1);
 
+        VueloEntity vueloEntity2 = new VueloEntity();
+        vueloEntity2.setDisponible(true);
+        vueloEntity2.setCapacidad(50);
+        vueloEntity2.setAerolinea("Aerolineas Uruguayas");
+        vueloEntity2.setHorarioPartida(LocalDateTime.parse("2023-06-25T23:53:30"));
+        vueloEntity2.setHorarioLlegada(LocalDateTime.parse("2023-06-25T23:53:30"));
+        vueloEntity2.setPrecio(15000.0);
+        vueloEntity2.setOrigen("Buenos Aires");
+        vueloEntity2.setDestino("Uruguay");
+        vueloEntities.add(vueloEntity2);
 
-        //ACT
-        List<VueloDTO> result = vueloService.obtenerVuelosDisponibles();
-        //ASSERT
-        assertEquals(vuelos,result);
+        when(vueloRepository.findByDisponibleTrue()).thenReturn(vueloEntities);
+
+        // Act
+        List<VueloDTO> vuelosObtenidos = vueloService.obtenerVuelosDisponibles();
+
+        // Assert
+        assertEquals(2, vuelosObtenidos.size());
+        assertEquals(vueloEntity1.getAerolinea(), vuelosObtenidos.get(0).getAerolinea());
+        assertEquals(vueloEntity2.getAerolinea(), vuelosObtenidos.get(1).getAerolinea());
+
+        //verify(vueloRepository, times(1)).findByDisponibleTrue();
     }
-
-
-
-
-
-}*/
+}
