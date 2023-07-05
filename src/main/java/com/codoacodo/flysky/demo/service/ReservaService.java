a399c2336bcdaca2549e933428ca659a50bab114
Mariano Favre <marianofavre79@yahoo.com.ar>
2023-07-05 14:42:05 -0300

    Agregar firma método obtenerNumeroVentasIngresosDiarios en interface ReservaService

--- src/main/java/com/codoacodo/flysky/demo/service/ReservaService.java
+++ src/main/java/com/codoacodo/flysky/demo/service/ReservaService.java
@@ -2,11 +2,16 @@ package com.codoacodo.flysky.demo.service;
 
 import com.codoacodo.flysky.demo.dto.request.ReservaVueloDTO;
 import com.codoacodo.flysky.demo.dto.response.ReservaVueloResponseDto;
+import com.codoacodo.flysky.demo.dto.response.VentaDTO;
+
+import java.time.LocalDate;
 
 
 public interface ReservaService {
 
     ReservaVueloResponseDto reservarVuelo(String nombreUsuarioTipoCliente, ReservaVueloDTO
             reservaVueloDTO);
+
+    VentaDTO obtenerNumeroVentasIngresosDiarios(String nombreUsuarioTipoAdministrador, LocalDate fecha);
 
 }
